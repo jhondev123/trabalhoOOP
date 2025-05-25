617a508c2a87e7bb4866678c8592e80112a72fcd
Jhonattan Curtarelli <Jhonattan.curtarelli@outlook.com>
2025-05-25 03:25:26 -0300

    feat: finalizando o cadastro do usuário e começando a criar o menu

--- src/main/java/com/TrabalhoOOP/App.java
+++ src/main/java/com/TrabalhoOOP/App.java
@@ -1,38 +1,68 @@
 package com.TrabalhoOOP;
+
 import com.TrabalhoOOP.Adapters.IbgeNoticeAdapter;
 import com.TrabalhoOOP.Controllers.NoticeController;
+import com.TrabalhoOOP.Controllers.UserController;
 import com.TrabalhoOOP.Entities.Notice;
+import com.TrabalhoOOP.Entities.User;
 import com.TrabalhoOOP.Interfaces.INoticesApi;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.net.http.HttpClient;
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
+
+    private final NoticeController noticeController;
+    private final UserController userController;
+    private final Scanner sc;
+
+    public App() {
+        this.sc = new Scanner(System.in);
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+        INoticesApi noticeApi = new IbgeNoticeAdapter(
+                httpClient,
+                new Gson()
+        );
+
+        this.noticeController = new NoticeController(noticeApi);
+        this.userController = new UserController();
+    }
+
     public static void main(String[] args) {
         try {
-            // variaveis de armazenamento em memória
-            List<Notice> notices;
-
-            // usando singleton para serviços
-            HttpClient httpClient = HttpClient.newHttpClient();
-            INoticesApi NoticeApi = new IbgeNoticeAdapter(
-                    httpClient,
-                    new Gson()
-            );
-            // controllers
-            NoticeController noticeController = new NoticeController(NoticeApi);
-
-            // consultando as noticias
-            notices = GetNoticies(noticeController);
-
-        }catch (Exception e){
+            App app = new App();
+            app.run();
+        } catch (Exception e) {
             System.out.println("Ocorreu um erro inesperado: " + e.getMessage());
         }
     }
-    public static List<Notice> GetNoticies(NoticeController controller) throws IOException, InterruptedException {
-        return controller.getAllNotices();
+
+    public void run() throws IOException, InterruptedException {
+        List<Notice> notices = getNotices();
+        User user = createUser();
+
+        showMenu(user);
+    }
+
+    private List<Notice> getNotices() throws IOException, InterruptedException {
+        return noticeController.getAllNotices();
+    }
+
+    private User createUser() {
+        System.out.print("Digite seu nome: ");
+        String name = sc.nextLine();
+        System.out.print("Digite seu apelido: ");
+        String nickName = sc.nextLine();
+        return userController.CreateUser(name, nickName);
+    }
+
+    private void showMenu(User user) {
+        System.out.println("Olá " + user.name + ", seja bem-vindo ao sistema!");
+        System.out.println("Você pode consultar as notícias, ou sair do sistema.");
+        System.out.println("Digite 1 para consultar as notícias, ou 0 para sair do sistema.");
     }
 }
--- src/main/java/com/TrabalhoOOP/Controllers/UserController.java
+++ src/main/java/com/TrabalhoOOP/Controllers/UserController.java
@@ -1,4 +1,10 @@
 package com.TrabalhoOOP.Controllers;
 
+import com.TrabalhoOOP.Entities.User;
+
 public class UserController {
+    public User CreateUser(String name, String nickName)
+    {
+        return new User(name, nickName);
+    }
 }
